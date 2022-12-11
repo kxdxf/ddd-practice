17e60f45248630268f061a71216e53273f7c928b
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2022-12-11 13:42:44 +0900

    feat: Nameオブジェクトを使用する形へ修正

--- spec/domain/full_name_spec.rb
+++ spec/domain/full_name_spec.rb
@@ -1,44 +1,54 @@
-require_relative '../../src/domain/full_name'
+require_relative '../../src/domain/name'
 
 RSpec.describe FullName, "#initialize" do
-  it 'インスタンスを初期化できること' do
-    fullname = FullName.new('keisuke', 'nakasuga')
-
+  it 'インスタンス化できること' do
+    fullname = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
     expect(fullname).to be_truthy
   end
 
   it 'first nameを取得できること' do
-    fullname = FullName.new('keisuke', 'nakasuga')
-
-    expect(fullname.first_name).to eq('keisuke')
+    fullname = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
+    expect(fullname.first_name.value).to eq('keisuke')
   end
 
   it 'last nameを取得できること' do
-    fullname = FullName.new('keisuke', 'nakasuga')
-
-    expect(fullname.last_name).to eq('nakasuga')
+    fullname = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
+    expect(fullname.last_name.value).to eq('nakasuga')
   end
 end
 
 RSpec.describe FullName, "#equal?" do
   it 'first_name, last_nameが等しい場合はtrueを返す' do
-    fullname_a = FullName.new('keisuke', 'nakasuga')
-    fullname_b = FullName.new('keisuke', 'nakasuga')
-
+    fullname_a = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
+    fullname_b = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
     expect(fullname_a.equal?(fullname_b)).to be_truthy
   end
 
   it 'first_nameが異なる場合はfalseを返す' do
-    fullname_a = FullName.new('keisuke', 'nakasuga')
-    fullname_b = FullName.new('k', 'nakasuga')
-
+    fullname_a = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
+    fullname_b = FullName.new(Name.new('k'), Name.new('nakasuga'))
     expect(fullname_a.equal?(fullname_b)).to be_falsey
   end
 
   it 'last_nameが異なる場合はfalseを返す' do
-    fullname_a = FullName.new('keisuke', 'nakasuga')
-    fullname_b = FullName.new('keisuke', 'n')
-
+    fullname_a = FullName.new(Name.new('keisuke'), Name.new('nakasuga'))
+    fullname_b = FullName.new(Name.new('keisuke'), Name.new('n'))
     expect(fullname_a.equal?(fullname_b)).to be_falsey
+  end
+end
+
+RSpec.describe Name, "#initialize" do
+  it 'インスタンス化できること' do
+    %w[keisuke nakasuga].each do |value|
+      expect(Name.new(value)).to be_truthy
+    end
+  end
+
+  it '引数がnilの場合はArgumentErrorを発生させる' do
+    expect{ Name.new(nil) }.to raise_error(ArgumentError)
+  end
+
+  it '引数に入力不可な文字が含まれている場合はArgumentErrorを発生させる' do
+    expect{ Name.new('n@kasuga') }.to raise_error(ArgumentError)
   end
 end
---
+++ src/domain/name.rb
@@ -0,0 +1,23 @@
+class FullName
+  attr_reader :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def equal?(other)
+    first_name.value == other.first_name.value &&
+      last_name.value == other.last_name.value
+  end
+end
+
+class Name
+  attr_reader :value
+
+  def initialize(value)
+    raise ArgumentError.new("#{value}") if value.nil?
+    raise ArgumentError.new("許可されていない文字が使われています。") unless value.match(/^[a-zA-Z]+$/)
+    @value = value
+  end
+end
