9d652e2a3e48f97250b76aaa21255f6f78ee1fa0
kxdxf <keisuke.nakasuga+githubXD@gmail.com>
2022-12-12 20:03:00 +0900

    feat: 4.4 エンティティや値オブジェクトと共にユースケースを組み立てる

--- spec/domain/user_spec.rb
+++ spec/domain/user_spec.rb
@@ -1,57 +1,67 @@
 require_relative '../../src/domain/user'
 
-RSpec.describe UserId, "#initiazize" do
-  it 'インスタンス化できること' do
-    expect(UserId.new(1)).to be_truthy
-  end
-
-  it 'idを取得できること' do
-    expect(UserId.new(1).value).to eq(1)
-  end
-
-  it '引数がnilの場合はArgumentErrorを発生させる' do
-    expect { UserId.new(nil) }.to raise_error(ArgumentError)
-  end
-end
-
 RSpec.describe User, "#initialize" do
   it 'インスタンス化できること' do
-    id = UserId.new(1)
-    expect(User.new(id, 'keisuke')).to be_truthy
+    expect(described_class.new('keisuke')).to be_truthy
   end
 
   it 'Userインスタンスは可変であること' do
-    id = UserId.new(1)
-    user = User.new(id, 'keisuke')
+    user = described_class.new('keisuke')
     user.change_name('nakasuga')
     expect(user.name).to eq('nakasuga')
   end
 
-  it '引数idがnilの場合はArgumentErrorを発生させる' do
-    expect { User.new(nil, 'keisuke') }.to raise_error(ArgumentError)
-  end
-
   it '引数nameがnilの場合はArgumentErrorを発生させる' do
-    id = UserId.new(1)
-    expect { User.new(id, nil) }.to raise_error(ArgumentError)
+    expect { described_class.new(nil) }.to raise_error(ArgumentError)
   end
 
   it '引数nameが3文字以下の場合はArgumentErrorを発生させる' do
-    id = UserId.new(1)
-    expect { User.new(id, 'ab') .to raise_error(ArgumentError) }
+    expect { described_class.new('ab') .to raise_error(ArgumentError) }
   end
 end
 
 RSpec.describe User, "#equals?" do
   it 'idが同じ場合はtrueを返す' do
-    user_a = User.new(UserId.new(1), 'keisuke')
+    user_a = described_class.new('keisuke')
     user_a_cp = user_a
     expect(user_a.equals?(user_a_cp)).to be_truthy
   end
 
   it 'idが異なる場合はfalseを返す' do
-    user_a = User.new(UserId.new(1), 'keisuke')
-    user_b = User.new(UserId.new(2), 'keisuke')
+    user_a = described_class.new('keisuke')
+    user_b = described_class.new('keisuke')
     expect(user_a.equals?(user_b)).to be_falsey
+  end
+end
+
+
+RSpec.describe UserId, "#initiazize" do
+  it 'インスタンス化できること' do
+    expect(described_class.new(1)).to be_truthy
+  end
+
+  it 'idを取得できること' do
+    expect(described_class.new(1).value).to eq(1)
+  end
+
+  it '引数がnilの場合はArgumentErrorを発生させる' do
+    expect { described_class.new(nil) }.to raise_error(ArgumentError)
+  end
+end
+
+
+RSpec.describe UserName, "#initialize" do
+  it 'インスタンス化できること' do
+    expect(described_class.new('kei')).to be_truthy
+  end
+
+  it '引数valueがnilの場合はArgumentErrorを発生させる' do
+    expect { described_class.new(nil) }.to raise_error(ArgumentError)
+  end
+
+  it '引数valueが3文字未満の場合はArgumentErrorを発生させる' do
+    %w[a aa].each do |value|
+      expect { described_class.new(value) }.to raise_error(ArgumentError)
+    end
   end
 end
--- src/domain/user.rb
+++ src/domain/user.rb
@@ -1,20 +1,12 @@
-class UserId
-  attr_reader :value
-
-  def initialize(value)
-    raise ArgumentError.new('valueがnilです。') if value.nil?
-    @value = value
-  end
-end
+require 'securerandom'
 
 class User
   attr_reader :id, :name
 
-  def initialize(id, name)
-    raise ArgumentError.new('idがnilです。') if id.nil?
+  def initialize(name)
     raise ArgumentError.new('nameがnilです。') if name.nil?
 
-    @id = id
+    @id = SecureRandom.uuid
     @name = name
   end
 
@@ -26,5 +18,24 @@ class User
 
   def equals?(other)
     self.equal?(other)
+  end
+end
+
+class UserId
+  attr_reader :value
+
+  def initialize(value)
+    raise ArgumentError.new('valueがnilです。') if value.nil?
+    @value = value
+  end
+end
+
+class UserName
+  attr_reader :value
+
+  def initialize(value)
+    raise ArgumentError.new("#{value}") if value.nil?
+    raise ArgumentError.new("ユーザー名は3文字以上です。") if value.size < 3
+    @value = value
   end
 end
